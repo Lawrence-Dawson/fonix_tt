33dd7b6625091fff836053fbcab50b4b9977ddcb
Lawrence Dawson <lawrencedawson@hotmail.co.uk>
2016-11-17 17:19:54 +0000

    finalising setup

---
+++ src/com/lawrence/techtest/Servlet.java
@@ -0,0 +1,22 @@
+package com.lawrence.techtest;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Servlet
+ */
+public class Servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		out.println("Hello Java!");
+	}
+
+}
