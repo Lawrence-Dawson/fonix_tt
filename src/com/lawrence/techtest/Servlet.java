b6a953d5a9e7ad438786071d88c4830ac02d8b9c
Lawrence Dawson <lawrencedawson@hotmail.co.uk>
2016-11-17 21:01:28 +0000

    server refuses to start

--- src/com/lawrence/techtest/Servlet.java
+++ src/com/lawrence/techtest/Servlet.java
@@ -16,7 +16,6 @@ public class Servlet extends HttpServlet {
        
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
-		out.println("Hello Java!");
+		getServletContext().getRequestDispatcher("/index.jsp").forward(request, response);
 	}
-
 }
--- src/com/lawrence/techtest/signIn.java
+++ src/com/lawrence/techtest/signIn.java
@@ -1,8 +1,6 @@
 package com.lawrence.techtest;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,16 +8,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class signUp
+ * Servlet implementation class signIn
  */
-//@WebServlet("/signUp")
-public class signUp extends HttpServlet {
+//@WebServlet("/signIn")
+public class signIn extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public signUp() {
+    public signIn() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -35,12 +33,7 @@ public class signUp extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		PrintWriter out = response.getWriter();
-		 String forename = request.getParameter("forename");
-	     String surname = request.getParameter("surname");
-	     
-	     out.println(forename);
-	     out.println(surname);
+	
 	}
 
 }
