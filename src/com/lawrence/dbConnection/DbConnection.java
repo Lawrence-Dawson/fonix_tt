895325a5291f360f15812bcb794246c8601ee97f
Lawrence Dawson <lawrencedawson@hotmail.co.uk>
2016-11-18 09:03:33 +0000

    added db connectivity

---
+++ src/com/lawrence/dbConnection/DbConnection.java
@@ -0,0 +1,34 @@
+package com.lawrence.dbConnection;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import com.mysql.jdbc.Connection;
+
+public class DbConnection {
+
+	public static void main(String[] args) {
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			
+			System.out.println("Driver found");
+		} catch (ClassNotFoundException e) {
+			
+			System.out.println("Driver not found + e");
+		}
+
+		String url="jdbc:mysql://localhost/fonixdb";
+		String user="root";
+		String password="";	
+		
+		Connection con=null;
+		try {
+		con=(Connection) DriverManager.getConnection(url, user, password);
+			System.out.println("Connected Successfully");
+		} catch (SQLException e){
+			System.out.println("Connection string error");
+		}
+	}
+
+}
--- src/com/lawrence/techtest/Servlet.java
+++ src/com/lawrence/techtest/Servlet.java
@@ -15,8 +15,7 @@ public class Servlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		PrintWriter out = response.getWriter();
-		out.println("Hello Java!");
+		getServletContext().getRequestDispatcher("/index.jsp").forward(request, response);
 	}
 
 }
