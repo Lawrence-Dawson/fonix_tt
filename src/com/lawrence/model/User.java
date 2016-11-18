cdbd216f748cb1b9f29ed99bcedbcb4cec0f7d9c
Lawrence Dawson <lawrencedawson@hotmail.co.uk>
2016-11-18 09:35:45 +0000

    added a user model

---
+++ src/com/lawrence/model/User.java
@@ -0,0 +1,13 @@
+package com.lawrence.model;
+
+public class User {
+
+	private int id;
+	private String forename;
+	private String surname;
+	private String email;
+	private String password;
+	
+
+	
+}
