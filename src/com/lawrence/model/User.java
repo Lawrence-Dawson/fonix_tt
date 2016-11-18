6192104cdf3bea9fedebb8003f46759944e3e663
Lawrence Dawson <lawrencedawson@hotmail.co.uk>
2016-11-18 09:36:26 +0000

    added a constructor and getter/setters to user model

--- src/com/lawrence/model/User.java
+++ src/com/lawrence/model/User.java
@@ -8,6 +8,45 @@ public class User {
 	private String email;
 	private String password;
 	
-
+	
+	public User(int id, String forename, String surname, String email, String password) {
+		super();
+		this.id = id;
+		this.forename = forename;
+		this.surname = surname;
+		this.email = email;
+		this.password = password;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getForename() {
+		return forename;
+	}
+	public void setForename(String forename) {
+		this.forename = forename;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
 	
 }
