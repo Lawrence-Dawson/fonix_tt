4e2474ec5967513302fea7e897da890d65b18402
Lawrence Dawson <lawrencedawson@hotmail.co.uk>
2016-11-25 22:46:38 +0000

    only unique users now add to db

--- src/com/lawrence/dao/DataAccess.java
+++ src/com/lawrence/dao/DataAccess.java
@@ -1,6 +1,7 @@
 package com.lawrence.dao;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import com.lawrence.dbConnection.DbConnection;
 import com.lawrence.model.User;
@@ -10,6 +11,9 @@ public class DataAccess {
 	public void addNew(User newUser) throws SQLException {
 		System.out.println("add new user function called");
 		try {
+			if (checkUnique(newUser.getEmail())) {
+				return;
+			} else {
 			System.out.println("Adding record");
 			PreparedStatement ps = DbConnection.getPreparedStatement("INSERT INTO USERS (Forename, Surname, Email, Password)" + "VALUES(?,?,?,?)");
 			ps.setString(1, newUser.getForename());
@@ -17,10 +21,26 @@ public class DataAccess {
 			ps.setString(3, newUser.getEmail());
 			ps.setString(4, newUser.getPassword());
 			ps.executeUpdate();
-
+			}
 		} catch (ClassNotFoundException e) {
 			System.out.println("Error adding record");
 		}
 		System.out.println("Record successfully added to database");
+	}
+
+	public boolean checkUnique(String email) throws ClassNotFoundException, SQLException {
+		final String queryCheck = "SELECT count(*) from USERS WHERE Email = ?";
+		final PreparedStatement ps = DbConnection.getPreparedStatement(queryCheck);
+		ps.setString(1, email);
+		final ResultSet resultSet = ps.executeQuery();
+		if(resultSet.next()) {
+		    final int count = resultSet.getInt(1);
+		    System.out.println( count );
+		    if (count > 0) {
+		    	System.out.println("user with email address " + email + " already present");
+		    	return true;
+		    }
+		}
+		return false;
 	}
 }
--- src/com/lawrence/techtest/signIn.java
+++ src/com/lawrence/techtest/signIn.java
@@ -2,11 +2,14 @@ package com.lawrence.techtest;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.lawrence.dao.DataAccess;
 
 /**
  * Servlet implementation class signIn
@@ -34,11 +37,18 @@ public class signIn extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		PrintWriter out = response.getWriter();
 		String email = request.getParameter("email");
 	    String password = request.getParameter("password");
-	    out.println(email);
-	    out.println(password);
+	    DataAccess database = new DataAccess();
+	    try {
+			database.checkUnique(email);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
